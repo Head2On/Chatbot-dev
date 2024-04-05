1dee336d2c6f2ede424fc83a3ee76fcae922fcc3
Ishan <147740148+Head2On@users.noreply.github.com>
2024-04-05 07:23:06 -0700

    Gui with tkinter

--- chat.py
+++ chat.py
@@ -1,37 +1,34 @@
 import random
 import json
+
 import torch
+
 from model import NeuralNet
-from nltk_utils import bag_of_words,tokenize
+from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-with open ('intents.json','r')as json_data:
+
+with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
-data= torch.load(FILE)
+data = torch.load(FILE)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
-all_words = data["all_words"]
-tags = data["tags"]
+all_words = data['all_words']
+tags = data['tags']
 model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Mili"
+bot_name = "Sam"
 
-print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
-
-    sentence = tokenize(sentence)
+def get_response(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -46,6 +43,8 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+                return random.choice(intent['responses'])
+    
+    return "I do not understand..."
+
+    
